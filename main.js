c4daf0c38938c0c12aa0ce32d07273f35383ba5e
Adomaitis Tadas <tadas.adomaitis@mif.stud.vu.lt>
2022-12-06 15:55:22 +0200

    new

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const { DiscordJS, Client, Intents, Collection, MessageEmbed } = require('discord.js');
-const client  = new Client({intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_PRESENCES]});
+const client = new Client({ intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILDS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.GUILD_PRESENCES] });
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 const prefix = '!DJ';
@@ -8,41 +8,41 @@ const mongoose = require('mongoose');
 const loading = require('./mongodb/loading');
 const endpoints = require('./webEndpoints');
 const express = require("express");
-const rateLimiter = require ('./middlewares/rateLimiter');
+const rateLimiter = require('./middlewares/rateLimiter');
 const cors = require('cors');
 var app = express();
 require('dotenv').config();
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const axios = require('axios');
 
-app.use(rateLimiter, cors({origin: '*'}));
+app.use(rateLimiter, cors({ origin: '*' }));
 
 app.listen(process.env.PORT || 80, () => {
     console.log("Server running on port " + process.env.PORT);
-   });
+});
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("ALIVE!");
-    });
+});
 
-app.get("/ping", (req,res) => {
+app.get("/ping", (req, res) => {
     res.send("PONG");
-    });
+});
 
 client.commands = new Collection();
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 const commands = [];
-for(const file of commandFiles){
+for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     commands.push(command.data.toJSON());
     client.commands.set(command.data.name, command);
     //client.commands.set(command.name, command);
 }
 mongoose.connect(process.env.MONGOOSE, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
 });
 
 mongoose.connection.on('connected', () => {
@@ -52,12 +52,12 @@ mongoose.connection.on('connected', () => {
 function ping() {
     console.log("ping");
     axios.get(process.env.BACKEND_LINK)
-    .then(response => {
-        console.log(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
 }
 
 //setInterval(ping, 5500);
@@ -75,7 +75,7 @@ client.once('ready', () => {
     }).setToken(process.env.DISCORD_KEY);
     (async () => {
         try {
-            if(process.env.ENV === "production"){
+            if (process.env.ENV === "production") {
                 await rest.put(Routes.applicationCommands(CLIENT_ID), {
                     body: commands
                 });
@@ -93,9 +93,9 @@ client.once('ready', () => {
 });
 
 client.on('interactionCreate', async interaction => {
-    if(!interaction.isCommand()) return;
+    if (!interaction.isCommand()) return;
     const command = client.commands.get(interaction.commandName);
-    if(!command) return;
+    if (!command) return;
 
     try {
         await command.execute(interaction);
@@ -108,6 +108,6 @@ client.on('interactionCreate', async interaction => {
     }
 });
 
-require('./messageSender.js')(client,DiscordJS);
+require('./messageSender.js')(client, DiscordJS);
 
 client.login(process.env.DISCORD_KEY);
--- sendEmail.js
+++ sendEmail.js
@@ -1,39 +1,39 @@
 const nodemailer = require('nodemailer');
 require('dotenv').config();
 module.exports = {
-    sendEmail (email, joke) {
+  sendEmail(email, joke) {
 
-        var transporter = nodemailer.createTransport({
-            host: 'smtp.hostinger.com',
-            port: '465',
-            auth: {
-                user: process.env.EMAIL,
-                pass: process.env.PASS
-            }
-        });
-    
-        var mailOptions = {
-            from: process.env.EMAIL,
-            to: email,
-            subject: 'Dienos Juokas',
-            text: 'Dienos Juokas!',
-            html: "<h1>Šiandienos juokas</h1>" +
-            "<p>"+ joke + "</p>" +
-            "<span>" + "Dienos juokas kiekvieną dieną!" + "</span>" +
-            "<br>"+
-            "<a href=\"https://www.dienosjuokas.tk\"><big>dienosjuokas.tk</big></a>" +
-            "<br>" + 
-            "<br>" + 
-            "<br>" + 
-            "<a href=\""+ process.env.BACKEND_LINK + "/unsubscribe?email="+email+"\"><small>Unsubscribe</small></a>",
-        };
-    
-        transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            } else {
-              console.log('Email sent: ' + info.response);
-            }
-          });
-    }
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.hostinger.com',
+      port: '465',
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
+      }
+    });
+
+    var mailOptions = {
+      from: process.env.EMAIL,
+      to: email,
+      subject: 'Dienos Juokas',
+      text: 'Dienos Juokas!',
+      html: "<h1>Šiandienos juokas</h1>" +
+        "<p>" + joke + "</p>" +
+        "<span>" + "Dienos juokas kiekvieną dieną!" + "</span>" +
+        "<br>" +
+        "<a href=\"https://www.dienosjuokas.tk\"><big>dienosjuokas.fun</big></a>" +
+        "<br>" +
+        "<br>" +
+        "<br>" +
+        "<a href=\"" + process.env.BACKEND_LINK + "/unsubscribe?email=" + email + "\"><small>Unsubscribe</small></a>",
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+      }
+    });
+  }
 }
